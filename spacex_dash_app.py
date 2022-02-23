3b298757667aadd6c123c8fc65473c447ccd16e4
Deep Manglik <manglikd@gmail.com>
2022-02-23 19:15:47 +0530

    Added pie chart and scatter plot to the dashboard

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -35,25 +35,62 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 html.Br(),
 
                                 # TASK 2: Add a pie chart to show the total successful launches count for all sites
-                                # If a specific launch site was selected, show the Success vs. Failed counts for the site
-                                html.Div(dcc.Graph(id='success-pie-chart')),
+                                # If a specific launch site was selected, show the Success vs. Failed counts for the site                        
+                                html.Div([ ], id='success-pie-chart'),
                                 html.Br(),
 
                                 html.P("Payload range (Kg):"),
                                 # TASK 3: Add a slider to select payload range
-                                #dcc.RangeSlider(id='payload-slider',...)
+                                dcc.RangeSlider(id='payload-slider', min=0, max=10000, step=1000,
+                                                marks={0: '0', 2500: '2500', 5000: '5000', 7500: '7500', 10000: '10000'},
+                                                value=[min_payload, max_payload]),
 
                                 # TASK 4: Add a scatter chart to show the correlation between payload and launch success
-                                html.Div(dcc.Graph(id='success-payload-scatter-chart')),
+                                html.Div([ ], id='success-payload-scatter-chart'),
                                 ])
 
 # TASK 2:
 # Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+# Function decorator to specify function input and output
+@app.callback([Output(component_id='success-pie-chart', component_property='children')],
+              [Input(component_id='site-dropdown', component_property='value')])
+def get_pie_chart(entered_site):
+    if entered_site == 'ALL':
+        succ_data = spacex_df[spacex_df['class'] == 1]
+        pie_fig = px.pie(succ_data, values='class', names='Launch Site', title='Total success launches by sites')
+        return [dcc.Graph(figure=pie_fig)]
+    else:
+        site_df = spacex_df[spacex_df['Launch Site'] == entered_site]
+        site_succ_count = (site_df['class']==1).sum()
+        site_fail_count = site_df.shape[0] - site_succ_count
+        
+        pie_fig = px.pie(values=[site_succ_count, site_fail_count], names=['Success', 'Failed'], title=entered_site + ' : Success vs. Failed')
+        return [dcc.Graph(figure=pie_fig)]          
 
-# TASK 4:
-# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 
+@app.callback([Output(component_id='success-payload-scatter-chart', component_property='children')],
+              [Input(component_id='site-dropdown', component_property='value'), Input(component_id="payload-slider", component_property="value")])
+def get_success_payload_chart(site, payload_range):
+    low, high = payload_range
+    
+    if site == 'ALL':
+        payload_mask = (spacex_df['Payload Mass (kg)'] > low) & (spacex_df['Payload Mass (kg)'] < high)
+        scatt_fig = fig = px.scatter(
+                                        spacex_df[payload_mask], x="Payload Mass (kg)", y="class", 
+                                        color="Booster Version Category", hover_data=['Booster Version'],
+                                        title='Correlation between Payload and Success for all sites'
+                                    )
+        return [dcc.Graph(figure=scatt_fig)]
+    else:
+        payload_mask = (spacex_df['Payload Mass (kg)'] > low) & (spacex_df['Payload Mass (kg)'] < high) & (spacex_df['Launch Site'] == site)        
+        scatt_fig = fig = px.scatter(
+                                        spacex_df[payload_mask], x="Payload Mass (kg)", y="class", 
+                                        color="Booster Version Category", hover_data=['Booster Version'],
+                                        title='Correlation between Payload and Success for site ' + site
+                                    )
+        return [dcc.Graph(figure=scatt_fig)]
 
+    
 # Run the app
 if __name__ == '__main__':
     app.run_server()
